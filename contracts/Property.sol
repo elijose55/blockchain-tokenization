e5a61c79b12d8708c51127b67c40808f4206c5f8
Eli Jose <elijose55@hotmail.com>
2019-12-01 17:52:35 -0300

    Add files via upload

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -7,18 +7,39 @@ contract Property is ERC20 {
 
 	address[] internal stakeholders;
 
+	string public name = "PropertyToken";
+	string public symbol = "RST";
+	uint8 public decimals = 3;
+	uint public INITIAL_SUPPLY = 1000;
+
+	uint public tokenSupply;
+	uint public propertyValue;
+	address payable public ownerAddress;
+	
+
+	constructor(address _owner, uint _value, uint _supply) public {
+
+	  _mint(_owner, _supply);
+	  require(_value > 0, "property value has to be greater than 0");
+	  require(_supply > 0, "token supply has to be greater than 0");
+
+	  propertyValue = _value;
+	  tokenSupply = _supply;
+	  ownerAddress = _owner;
+
+
+	}
+
 	// Adopting a pet
 	function adopt(uint petId) public returns (uint) {
-	require(petId >= 0 && petId <= 15);
-
-	adopters[petId] = msg.sender;
-
-	return petId;
+		require(petId >= 0 && petId <= 15);
+		adopters[petId] = msg.sender;
+		return petId;
 	}
 
 	// Retrieving the adopters
 	function getAdopters() public view returns (address[16] memory) {
-	return adopters;
+		return adopters;
 	}
 
 }
